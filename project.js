7c321758eda7e0bcca3f381bf9e9237883aef662
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-25 13:18:37 +1100

    created spin function, randomises symbols

--- project.js
+++ project.js
@@ -74,8 +74,40 @@ const getBet = (balance, lines) => {
     }
 }
 
+// 4. Spin slot machine
+const spin = () => {
+    // how many symbols we have
+    // even though empty, still a const, can manipulate whats inside, without changing ref to array
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    // create 3 nested arrays, arrays inside arrays
+    // each array reps a column
+    const reels = [[], [], []]
+    for (let i = 0; i < COLS; i++) {
+        // removes symbols already used
+        const reelSymbols = [...symbols];
+        for (let j =0; j < ROWS; j++){
+            // math.random take number between 0 and 1, multiply by max amount of symbols
+            // math.floor round down to lowest whole number, stop us from giving us out of array
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1); // remove one element
+        }
+    }
+    return reels;
+};
+
+
+
 // starting balance based on deposit amount, will change based on their amount win/ lose
 // let so can adjust variable
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
