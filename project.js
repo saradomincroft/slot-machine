650bf736a9b2f646de80c544bd43e8e81b0136c2
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-25 12:56:40 +1100

    added function getBet, which takes balance and lines as arguments. This is structured similar to previous functions

--- project.js
+++ project.js
@@ -8,6 +8,7 @@
 
 const prompt = require("prompt-sync")();
 
+// 1. Deposit some money
 const deposit = () => {
     // Put this in infinite loop, loop forever
     // ask user until give us number amount
@@ -24,12 +25,11 @@ const deposit = () => {
     }
 };
 
+// 2. Determine number of lines to bet on
 const getNumberOfLines =() => {
     while (true) {
-        const lines = prompt("Enter the number of lines to bet on: ")
+        const lines = prompt("Enter the number of lines to bet on (1-3): ")
         const numberOfLines = parseFloat(lines);
-        // default returned as strong, we need to convert to number
-        // Convert to floating point (like parseInt for intergers)
         if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
             console.log("Invalid number of lines, try again.");
         } else {
@@ -38,6 +38,23 @@ const getNumberOfLines =() => {
     }
 };
 
-const depositAmount = deposit();
+// 3. Collect a bet amount
+// balance as parameter, use to determine max bet
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the total bet per line: ")
+        const numberBet = parseFloat(bet);
+
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again.");
+        } else {
+            return numberBet;
+        }
+    }
+}
+
+// starting balance based on deposit amount, will change based on their amount win/ lose
+// let so can adjust variable
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
-console.log(depositAmount);
+const bet = getBet(balance, numberOfLines);
